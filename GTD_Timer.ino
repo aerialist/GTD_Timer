cd3cc10176454a82e716438e28128f290595a315
Shunya Sato <shunya.sato.310@gmail.com>
2016-03-19 17:42:04 +0900

    Got two minutes count up working! Implement Serial for debug.

--- GTD_Timer.ino
+++ GTD_Timer.ino
@@ -1,34 +1,19 @@
 /*
-  LED bar graph
+  GTD Timer
 
-  Turns on a series of LEDs based on the value of an analog sensor.
-  This is a simple way to make a bar graph display. Though this graph
-  uses 10 LEDs, you can use any number by changing the LED count
-  and the pins in the array.
+  Use 10 LED Bar to count two minutes.
 
-  This method can be used to control any series of digital outputs that
-  depends on an analog input.
-
-  The circuit:
-   * LEDs from pins 2 through 11 to ground
-
- created 4 Sep 2010
- by Tom Igoe
-
- This example code is in the public domain.
-
- http://www.arduino.cc/en/Tutorial/BarGraph
  */
 
 
 // these constants won't change:
-const int analogPin = A0;   // the pin that the potentiometer is attached to
 const int ledCount = 10;    // the number of LEDs in the bar graph
 const int buttonPin = 13;   // the pin that the button is attached to
 
 int ledPins[] = {
   2, 3, 4, 5, 6, 7, 8, 9, 10, 11
 };   // an array of pin numbers to which LEDs are attached
+unsigned long previousMillis = 0;
 
 uint16_t pattern0   = (B00000000 * 256) + B00000000; // Binary formatter only works for 8 bit values
 uint16_t pattern10  = (B00000000 * 256) + B00000001;
@@ -41,13 +26,12 @@ uint16_t pattern70  = (B00000000 * 256) + B01111111;
 uint16_t pattern80  = (B00000000 * 256) + B11111111;
 uint16_t pattern90  = (B00000001 * 256) + B11111111;
 uint16_t pattern100 = (B00000011 * 256) + B11111111;
-uint16_t pattern110 = (B00000011 * 256) + B11111111;
 
 void dynamicDrive(uint16_t pattern){
   for (int thisLed = 0; thisLed < ledCount; thisLed++){
     if (pattern & (1<<thisLed)){  // test pattern's thisLed-th bit
       digitalWrite(ledPins[thisLed], HIGH);
-      delay(1); // 3ms makes flicker, 0ms dims
+      delay(1); // 3ms makes flicker, 0ms dims for 16MHz
       digitalWrite(ledPins[thisLed], LOW);
     }
   }
@@ -60,6 +44,7 @@ void allOFF(){
 }
 
 void setup() {
+  Serial.begin(57600);
   // loop over the pin array and set them all to output:
   for (int thisLed = 0; thisLed < ledCount; thisLed++) {
     pinMode(ledPins[thisLed], OUTPUT);
@@ -69,7 +54,95 @@ void setup() {
 }
 
 void loop() {
-  dynamicDrive(pattern90);
+  unsigned long currentMillis = millis();
+  if (digitalRead(buttonPin)){
+    // button is pressed!
+    previousMillis = currentMillis;
+  }
+  unsigned long elapsedTime = currentMillis - previousMillis;
+  
+  if (elapsedTime > 180000){
+    // three minutes has passed.
+    // go to sleep
+    Serial.print(elapsedTime);
+    Serial.println(": >180 Going to sleep");
+    allOFF();
+  } else if (elapsedTime > 120000){
+    // two minutes has passed.
+    // run final dance
+    Serial.print(elapsedTime);
+    Serial.println(": >120");
+    // alternate by a second
+    if ((elapsedTime / 1000) % 2){
+      dynamicDrive(pattern0);
+    } else {
+      dynamicDrive(pattern100);
+    }
+  } else if (elapsedTime > 110000){
+    Serial.print(elapsedTime);
+    Serial.println(": >110");
+    // alternate by a 100 ms
+    if ((elapsedTime / 100) % 2){
+      dynamicDrive(pattern90);
+    } else {
+      dynamicDrive(pattern100);
+    }
+  } else if (elapsedTime > 100000){
+    Serial.print(elapsedTime);
+    Serial.println(": >100");
+    // alternate by a second
+    if ((elapsedTime / 1000) % 2){
+      dynamicDrive(pattern90);
+    } else {
+      dynamicDrive(pattern100);
+    }
+  } else if (elapsedTime > 90000){
+    Serial.print(elapsedTime);
+    Serial.println(": >90");
+    dynamicDrive(pattern90);
+  } else if (elapsedTime > 80000){
+    Serial.print(elapsedTime);
+    Serial.println(": >80");
+    dynamicDrive(pattern80);
+  } else if (elapsedTime > 70000){
+    Serial.print(elapsedTime);
+    Serial.println(": >70");
+    dynamicDrive(pattern70);
+  } else if (elapsedTime > 60000){
+    Serial.print(elapsedTime);
+    Serial.println(": >60");
+    dynamicDrive(pattern60);
+  } else if (elapsedTime > 50000){
+    Serial.print(elapsedTime);
+    Serial.println(": >50");
+    dynamicDrive(pattern50);
+  } else if (elapsedTime > 40000){
+    Serial.print(elapsedTime);
+    Serial.println(": >40");
+    dynamicDrive(pattern40);
+  } else if (elapsedTime > 30000){
+    Serial.print(elapsedTime);
+    Serial.println(": >30");
+    dynamicDrive(pattern30);
+  } else if (elapsedTime > 20000){
+    Serial.print(elapsedTime);
+    Serial.println(": >20");
+    dynamicDrive(pattern20);
+  } else if (elapsedTime > 10000){
+    Serial.print(elapsedTime);
+    Serial.println(": >10");
+    dynamicDrive(pattern10);
+  } else {
+    // less than 10 sec
+    Serial.print(elapsedTime);
+    Serial.println(": Less than 10");
+    // alternate by a second
+    if ((elapsedTime / 1000) % 2){
+      dynamicDrive(pattern0);
+    } else {
+      dynamicDrive(pattern10);
+    }
+  }
 }
 
 
